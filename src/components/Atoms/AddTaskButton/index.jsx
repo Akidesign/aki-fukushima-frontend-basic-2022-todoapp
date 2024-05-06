6dd5369ed3392d56da7b8cc4caeb08888d7e7548
Akidesign <autumncherry@outlook.jp>
2024-05-06 15:42:54 +0900

    スタイル以外入力

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import plus from "../../../assets/svg/plus.svg";
+import TEXT from "../../../variables/texts";
+import BREAKPOINT from "../../../variables/breakpoint";
+import FONTFAMILY from "../../../variables/font_family";
 
-export const Title = () => {
-  return null;
+export const AddTaskButton = ({ onClick }) => {
+  return (
+    <Button onClick={onClick}>
+      <PlusButton src={plus} />
+      <AddTaskText>タスクを追加</AddTaskText>
+    </Button>
+  );
 };
+
+const Button = styled.button``;
