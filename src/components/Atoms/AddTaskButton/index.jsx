9b94ba194f19634a4581a8823c1452e2ada36f3a
Akidesign <autumncherry@outlook.jp>
2024-05-08 10:32:57 +0900

    コード入力

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import plus from "../../../assets/svg/plus.svg";
 import TEXT from "../../../variables/texts";
-import BREAKPOINT from "../../../variables/breakpoint";
 import FONTFAMILY from "../../../variables/font_family";
 
 export const AddTaskButton = ({ onClick }) => {
@@ -15,4 +14,28 @@ export const AddTaskButton = ({ onClick }) => {
   );
 };
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: ${COLOR.GREEN_TRANSPARENT};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  gap: 10px;
+  border: none;
+  border-radius: 12px;
+  :hover {
+    background-color: ${COLOR.GREEN_TRANSLUCENT};
+  }
+`;
+
+const PlusButton = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const AddTaskText = styled.p`
+  ${TEXT.S}
+  color: ${COLOR.GREEN_TRANSPARENT};
+  font-family: ${FONTFAMILY.NOTO_SANS_JP};
+  margin: 0;
+`;
--- src/components/Atoms/AddTaskButton/index.stories.jsx
+++ src/components/Atoms/AddTaskButton/index.stories.jsx
@@ -3,7 +3,7 @@ import { AddTaskButton } from "./index";
 export default { component: AddTaskButton };
 
 export const Default = {
-    args: {
-        onclick: () => console.log("clicked"),
-    };
+  args: {
+    onclick: () => console.log("clicked"),
+  },
 };
