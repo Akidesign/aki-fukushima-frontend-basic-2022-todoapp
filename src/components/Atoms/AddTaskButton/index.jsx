d1d059a7140dc77480c84ffa36beafdc5526901a
Akidesign <autumncherry@outlook.jp>
2024-05-08 13:22:29 +0900

    コード完成

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import plus from "../../../assets/svg/plus.svg";
 import TEXT from "../../../variables/texts";
-import BREAKPOINT from "../../../variables/breakpoint";
 import FONTFAMILY from "../../../variables/font_family";
 
 export const AddTaskButton = ({ onClick }) => {
@@ -15,4 +14,31 @@ export const AddTaskButton = ({ onClick }) => {
   );
 };
 
-const Button = styled.button``;
+const Button = styled.button`
+  padding: 2px 6px;
+  border: 0px;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  gap: 10px;
+  cursor: pointer;
+  background-color: ${COLOR.GREEN_TRANSPARENT};
+  :hover {
+    background-color: ${COLOR.GREEN_TRANSLUCENT};
+    border-radius: 12px;
+  }
+`;
+
+const PlusButton = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const AddTaskText = styled.p`
+  ${TEXT.S}
+  color: ${COLOR.GREEN};
+  font-family: ${FONTFAMILY.NOTO_SANS_JP};
+  margin: 0;
+`;
