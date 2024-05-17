3a6ed002fd72d6fb2ca9735bf4cac3306fbe2cd7
Aki Fukushima <autumncherry@outlook.jp>
2024-05-17 23:53:34 +0900

    とりあえず入力　細かい修正はあるが、進行状況保存のため、コミット

--- src/components/Atoms/Input/index.stories.jsx
+++ src/components/Atoms/Input/index.stories.jsx
@@ -2,4 +2,11 @@ import { Input } from "./index";
 
 export default { component: Input };
 
-export const Default = {};
+export const Default = {
+  args: {
+    onEditComplete: (value) => {
+      console.log("edit complete: + value");
+    },
+    defaultValue: "",
+  },
+};
