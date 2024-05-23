16745645a9d1617e974e11b824a82df25d5a3abd
Aki Fukushima <autumncherry@outlook.jp>
2024-05-23 12:58:01 +0900

    style入力と細かい部分の修正

--- src/components/Molecules/Task/index.jsx
+++ src/components/Molecules/Task/index.jsx
@@ -33,7 +33,7 @@ export const Task = ({
         <Input onEditComplete={onEditComplete} defaultValue={taskName} />
       ) : (
         <StyledNameAndButtonContainer>
-          <StyledTaskName>{TaskName}</StyledTaskName>
+          <StyledTaskName>{taskName}</StyledTaskName>
           <StyledEditButtonContainer>
             <EditButton onClick={onEditButtonClick} />
           </StyledEditButtonContainer>
@@ -45,14 +45,37 @@ export const Task = ({
 export default Task;
 
 const StyledContainer = styled.div`
-  display: flex;
-  width: 256px;
   padding: 2px 6px;
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
+  flex: 1 0 0;
+`;
+
+const StyledCheckboxContainer = styled.div`
+  display: flex;
+  width: 20px;
+  height: 20px;
+  justify-content: center;
   align-items: center;
+`;
+
+const StyledNameAndButtonContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
   gap: 10px;
 `;
 
-const StyledCheckboxContainer = styled.div``;
-const StyledNameAndButtonContainer = styled.div``;
-const StyledTaskName = styled.div``;
-const StyledEditButtonContainer = styled.div``;
+const StyledTaskName = styled.div`
+  color: ${COLOR.LIGHT_GRAY};
+  font-family: ${FONT_FAMILY.NOTO_SANS};
+  ${TEXT.S}
+  margin-right: 10px;
+  flex: 1;
+`;
+
+const StyledEditButtonContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 4px;
+`;
