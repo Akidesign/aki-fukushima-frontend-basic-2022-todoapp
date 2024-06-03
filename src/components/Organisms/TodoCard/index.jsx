bb077950ff7530c000cd09cf946701e89d85860f
Aki Fukushima <autumncherry@outlook.jp>
2024-06-03 16:59:51 +0900

    エラーと細かい部分の修正

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -4,60 +4,47 @@ import { Task } from "../../Molecules/Task/index";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
 
-export const TodoCard = () ={
+export const TodoCard = () => {
+  const [taskList, setTaskList] = useState([]);
 
-const [IsTaskList, setIsTaskList] = useState([]);
+  const onAddTaskButtonClick = () => {
+    setTaskList([...taskList, { name: "", initializing: true }]);
+  };
 
-const onAddTaskButtonClick = () => 
-  {
-  setTaskList([...taskList, { name: "", initializing: true }]);
+  const onTaskComplete = (popTargetIndex) => {
+    setTaskList(taskList.filter((task, index) => index !== popTargetIndex));
+  };
+
+  const onTaskNameChange = (value, editTargetIndex) => {
+    if (value === "") {
+      onTaskComplete(editTargetIndex);
+    } else {
+      setTaskList(() => {
+        const newTaskList = [...(taskList || [])];
+        newTaskList[editTargetIndex].name = value;
+        newTaskList[editTargetIndex].initializing = false;
+        return newTaskList;
+      });
+    }
+  };
+
+  return (
+    <StyledContainer>
+      <AddTaskButton onClick={onAddTaskButtonClick} />
+      <StyledTaskList>
+        {taskList.map((task, index) => (
+          <Task
+            key={index}
+            onTaskNameChange={(value) => onTaskNameChange(value, index)}
+            onTaskComplete={() => onTaskComplete(index)}
+            taskName={task.name}
+            defaultValue={task.initializing}
+          />
+        ))}
+      </StyledTaskList>
+    </StyledContainer>
+  );
 };
-
-const onTaskComplete = (popTargetIndex) => 
-  {
-  setTaskList(taskList.filter((task, index) => (index !== popTargetIndex)))
-};
-
-const onTaskNameChange = (value, editTargetIndex) => {
-  if(value === ""){
-    onTaskComplete(editTargetIndex);
-  }
-  
-  else{
-    setTaskList(() => {
-      const newTaskList = [...(taskList || [])];
-      newTaskList[editTargetIndex].name = value;
-      newTaskList[editTargetIndex].initializing = false;
-      return newTaskList;
-    })
-  }
-};
-
-};
-
-return (
-<styledContainer>
-    <AddTaskButton onClick={onAddTaskButtonClick} />
-    <StyledTaskList>
-    {taskList.map((task, index) => (
-      <TaskContainer key={index}>
-       <Task 
-       key = {index}
-       onTaskNameChange= {(value) => onTaskNameChange(value, index)}
-       onTaskComplete = {() => onTaskComplete(index)}
-       taskName = {task.name}
-       defaultValue = {task.initializing} />
-      </TaskContainer>
-    ))}
-   </AddTaskButton>
-   {IsTaskList ? (
-    <onTaskNameChange
-   )}
-   </StyledTaskList>
-  </styledContainer>
-);
-
-export default TodoCard;
 
 const StyledContainer = styled.div`
   gap: 10px;
@@ -67,12 +54,11 @@ const StyledContainer = styled.div`
   flex-direction: column;
   border-radius: 4px;
   background-color: ${COLOR.LIGHT_BLACK};
-`
+`;
 
 const StyledTaskList = styled.div`
   display: flex;
   align-self: stretch;
   flex-direction: column;
   color: ${COLOR.LIGHT_GRAY};
-  ${TEXT.S}
-`
+`;
