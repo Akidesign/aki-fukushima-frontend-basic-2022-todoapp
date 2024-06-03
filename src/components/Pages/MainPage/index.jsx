cd5cc3184b2cfd19f112bd4f5f3abc12fb20652a
Aki Fukushima <autumncherry@outlook.jp>
2024-06-03 18:34:08 +0900

    ファイル作成とひな形入力

---
+++ src/components/Pages/MainPage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Title } from "../../Atoms/Title";
+import { TodoCard } from "../../Organisms/TodoCard";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+
+export const MainPage = () => {
+    return ();
+};
+
