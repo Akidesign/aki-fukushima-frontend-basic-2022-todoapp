c896f43a83e072298a8afbb778ce3fa6e42364c6
Aki Fukushima <autumncherry@outlook.jp>
2024-06-04 14:28:10 +0900

    コード入力

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -2,9 +2,36 @@ import React from "react";
 import { Title } from "../../Atoms/Title";
 import { TodoCard } from "../../Organisms/TodoCard";
 import styled from "styled-components";
-import COLOR from "../../../variables/color";
+import BREAKPOINT from "../../../variables/breakpoint";
 
 export const MainPage = () => {
-    return ();
+  return (
+    <StyledMainPageContainer>
+      <Title />
+      <StyledTodoCardContainer>
+        <TodoCard />
+      </StyledTodoCardContainer>
+    </StyledMainPageContainer>
+  );
 };
 
+const StyledMainPageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  gap: 20px;
+
+  @media (min-width: ${BREAKPOINT.MEDIUM}) {
+    padding: 20px;
+  }
+`;
+
+const StyledTodoCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  width: 500px;
+`;
